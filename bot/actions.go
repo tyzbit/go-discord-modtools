b2df3faa63fc61d7325e6c8c48f5b43cf540519c
Tyler Hawkins <3319104+tyzbit@users.noreply.github.com>
2023-11-21 15:26:36 -0500

    fix: (actions) logging

--- bot/actions.go
+++ bot/actions.go
@@ -135,7 +135,7 @@ func (bot *ModeratorBot) UpdateModeratedUser(u ModeratedUser) error {
 	tx := bot.DB.Model(&ModeratedUser{}).Where(&ModeratedUser{UserID: u.UserID}).Updates(u)
 
 	if tx.RowsAffected != 1 {
-		return fmt.Errorf("did not update one user row as expected, updated %v rows", tx.RowsAffected)
+		return fmt.Errorf("did not update one user row as expected, updated %v rows for user %s(%s)", tx.RowsAffected, u.UserName, u.UserID)
 	}
 	return nil
 }
