8c5eb8e142355759e8bda00ac33a347ffeaf968e
Tyler Hawkins <3319104+tyzbit@users.noreply.github.com>
2023-11-24 15:28:11 -0500

    feat: (message commands) add some evidence info

--- bot/commands_message.go
+++ bot/commands_message.go
@@ -1,6 +1,9 @@
 package bot
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/bwmarrin/discordgo"
 	log "github.com/sirupsen/logrus"
 	"github.com/tyzbit/go-discord-modtools/globals"
@@ -88,13 +91,67 @@ func (bot *ModeratorBot) GetUserInfoFromMessageContext(i *discordgo.InteractionC
 }
 
 func (bot *ModeratorBot) SaveEvidenceFromMessageContext(i *discordgo.InteractionCreate) {
+	message := *i.Interaction.ApplicationCommandData().Resolved.Messages[i.ApplicationCommandData().TargetID]
+
+	fields := []*discordgo.MessageEmbedField{
+		{
+			Name:   "Original message timestamp",
+			Value:  fmt.Sprintf("%s (<t:%v:R>)", message.Timestamp.Format(time.RFC1123Z), message.Timestamp.Unix()),
+			Inline: false,
+		},
+		{
+			Name:   "Author of message",
+			Value:  fmt.Sprintf("<@%s>", message.Author.ID),
+			Inline: true,
+		},
+		{
+			Name:   "Link to original message",
+			Value:  fmt.Sprintf("https://discord.com/channels/%s/%s/%s", i.GuildID, message.ChannelID, message.ID),
+			Inline: true,
+		},
+		{
+			Name:  "Content of original message",
+			Value: message.Content,
+		},
+	}
+	if len(message.Attachments) > 0 {
+		fields = append(fields, &discordgo.MessageEmbedField{
+			Name:  "Attachments",
+			Value: fmt.Sprintf("%v", len(message.Attachments)),
+		})
+		for _, attachment := range message.Attachments {
+			fields = append(fields, &discordgo.MessageEmbedField{
+				Name:  attachment.Filename,
+				Value: attachment.URL,
+			})
+		}
+	}
+	// TODO: more information
+	ms := discordgo.MessageSend{
+		Embeds: []*discordgo.MessageEmbed{{
+			Title: "Moder8s Evidence",
+			Description: fmt.Sprintf("Collected by <@%s> on %s (<t:%v:R>)",
+				i.Interaction.Member.User.ID,
+				time.Now().Format(time.RFC1123Z),
+				time.Now().Unix(),
+			),
+			Fields: fields,
+			Color:  globals.Purple,
+		}},
+	}
+
 	sc := bot.getServerConfig(i.GuildID)
+	_, err := bot.DG.ChannelMessageSendComplex(sc.EvidenceChannelSettingID, &ms)
+	if err != nil {
+		log.Warn("Unable to send message %w", err)
+	}
 
 	// TODO: more information
 	_ = bot.DG.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
 			Content: "Evidence saved to <#" + sc.EvidenceChannelSettingID + ">",
+			Flags:   discordgo.MessageFlagsEphemeral,
 		},
 	})
 }
